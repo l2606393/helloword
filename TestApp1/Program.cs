c5d77a6060ee5c85c1cf0ce19517b0352740f7de
l2607099 <l2607099@mvrht.com>
2017-02-15 11:45:15 +0700

    Update Program.cs

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -11,6 +11,7 @@ namespace TestApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
+            Console.WriteLine("Ahahhaha");
             Console.ReadKey();
         }
     }
